06f09cd1847f2e1b03a76faf3a5b88e9d35e435e
Brett Cherrington <brett@annalytics.co.uk>
2023-12-31 11:23:01 +0000

    Readme

--- infra/stack.ts
+++ infra/stack.ts
@@ -3,19 +3,21 @@ import * as cdk from 'aws-cdk-lib';
 import { ReactAppStack } from './react-app-stack';
 import { getResourceName } from './utils';
 import { CertificateStack } from './certificate-stack';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 const appName = 'annalytics-website';
-const stage = 'dev';
-const domain = 'annalytics.co.uk';
-const websiteDomain = 'www.annalytics.co.uk';
-const ecoDevAccount = '063191902099';
+const stage = 'dev' || process.env.DEPLOYMENT_STAGE!!;
+const domain = process.env.DOMAIN!!;
+const websiteDomain = process.env.WEBSITE_DOMAIN!!;
+const awsAccount = process.env.AWS_ACCOUNT!!;
 const primaryRegion = 'us-east-1' // cloudfront certificate have to be in us-east-1...!?!?
 
 const app = new cdk.App();
 
 // These two stacks are separated out here so you 'can' deploy them a different regions if you want to but at the moment there is no need
 const certStack = new CertificateStack(app,  getResourceName(appName, stage, 'cert-stack'), {
-  env: { account: ecoDevAccount, region: primaryRegion },
+  env: { account: awsAccount, region: primaryRegion },
   crossRegionReferences: true,
   appName,
   stage,
@@ -24,7 +26,7 @@ const certStack = new CertificateStack(app,  getResourceName(appName, stage, 'ce
 })
 
 new ReactAppStack(app, getResourceName(appName, stage, 'stack'), {
-  env: { account: ecoDevAccount, region: primaryRegion },
+  env: { account: awsAccount, region: primaryRegion },
   crossRegionReferences: true,
   appName,
   stage,
